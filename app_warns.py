e9a4aeb2e93bddab6dd6570267183ddf81f27462
Omar <omar.ali.alsuwaidi@gmail.com>
2023-03-12 21:52:34 +0400

    Performance improvements

--- app_warns.py
+++
@@ -1,6 +0,0 @@
-# بِسْمِ ٱللَّٰهِ ٱلرَّحْمَٰنِ ٱلرَّحِيمِ و به نستعين
-
-# Define some semantic warnings:
-acc_exist = 'This email already exists'
-acc_no_exist = 'This email address does not exist'
-wrong_pass = 'The password you’ve entered is incorrect'
